01029dfed55d7290c5ed7a8e6dccdab5fddc07c7
Chandra <chandra@chandra.(none)>
2014-01-30 11:26:42 +0530

    Form Disabled

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,15 @@
 class UserMailer < ActionMailer::Base
   default from: "itsupport@vivedaglobal.com"
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.password_reset.subject
   #
   def himail(user)
-    mail(:to => "#{user.name} <chandrasekharjangam@gmail.com>", :subject => "Registered")
+   data = File.read(Rails.root.join('public/assets/home2.png'))
+   puts Rails.root.join('public/assets/home2.png')
+    attachments.inline['header.png'] = data
+    mail(:to => "#{user.name} <chandrasekharjangam@gmail.com>", :subject => "Registered",:content_type => 'text/html')
   end
 
   def password_reset
